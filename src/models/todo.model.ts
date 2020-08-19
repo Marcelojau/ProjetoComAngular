d397b8fe960ab6a64e796d3db4db2de6bc6debb9
Marcelo Gomes <marcelogn2010@hotmail.com>
2020-08-19 00:46:56 -0300

    Primeiro Commit

---
+++ src/models/todo.model.ts
@@ -0,0 +1,13 @@
+export class Todo {
+    //export fala que a classe é publica
+
+    constructor(
+        public id: Number, 
+        public title: String,
+        public done: Boolean
+        
+        
+    ){}
+    
+
+}
